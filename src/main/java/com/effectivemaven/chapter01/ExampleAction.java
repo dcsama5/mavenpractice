95e6c6b6099cd79b2893a4682691b8d6001b0f72
Ameer <dcs.ama5@gmail.com>
2013-10-14 13:46:33 +0100

    another update

--- src/main/java/com/effectivemaven/chapter01/ExampleAction.java
+++ src/main/java/com/effectivemaven/chapter01/ExampleAction.java
@@ -5,6 +5,7 @@ import org.slf4j.*;
 public class ExampleAction {
 	
 	final Logger logger = LoggerFactory.getLogger(ExampleAction.class);
+	private java.util.List<String> values;
 	
 	public boolean execute() {
 		logger.info("Example action executed");
